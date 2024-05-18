c00159c90405da73c69744b4c9abee7b267daf94
Jiyun <kjy5952@naver.com>
2024-05-18 23:01:22 +0900

    Backjun Greedy 1541 add explanation

--- Backjun/Greedy Algorithm/1541.py
+++ Backjun/Greedy Algorithm/1541.py
@@ -1,19 +1,18 @@
 # 잃어버린 괄호
 
-exp = input().split('-') #'-'를 기준으로 분리하기
-print(exp)
+exp = input().split('-') #'-'를 기준으로 분리하여 리스트에 저장
 
 num = [] # '-'로 나눈 각 항들의 합을 저장하는 리스트
 
 for i in exp:
     sum = 0
     tmp = i.split('+') # 수를 알기 위해 + 기준으로 분리
-    for j in tmp:
-        sum += int(j)
-    num.append(sum)
+    for j in tmp: 
+        sum += int(j)  # 각 수를 더하기
+    num.append(sum)  # 더한 결과값을 num에 저장
 
-n = num[0]
+n = num[0] #항들의 합의 첫번째 원소를 n에 저장
 
 for i in range(1, len(num)):
-    n -= num[i]
+    n -= num[i] 
 print(n)
