503d6ec06aeaf95f4114461695e4c4dd0a2fb4d5
Jiyun <kjy5952@naver.com>
2024-07-13 05:18:45 +0900

    Baekjoon Advanced 1316 (2)

--- Baekjoon/Advanced/1316.py
+++ Baekjoon/Advanced/1316.py
@@ -1,8 +1,15 @@
 # 그룹 단어 체커
 
 n = int(input())
-arr = [0]*n
+cnt = n
 
 for i in range(n):
-    arr[i] = input()
+    word = input()
+    for j in range(0, len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]: #바로 뒤의 문자와 다르면서 뒤의 문자들 중에 같은 문자가 존재하는 경우
+            cnt -= 1
+            break
 
+print(cnt)
