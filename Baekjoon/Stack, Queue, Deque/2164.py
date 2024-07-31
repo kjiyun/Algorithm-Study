0f450d16edf9bea898d4962f54b2228177b57263
Jiyun <kjy5952@naver.com>
2024-07-31 12:23:51 +0900

    Baekjoon Queue 2164(2)

--- Baekjoon/Stack, Queue, Deque/2164.py
+++ Baekjoon/Stack, Queue, Deque/2164.py
@@ -12,3 +12,18 @@ while len(card) > 1:
     card.append(card.popleft())
 
 print(card[0])
+
+#Queue() 클래스를 사용한 방법 -> 틀림
+import queue
+
+card = queue.Queue()
+n = int(input())
+
+for i in range(n):
+    card.put(i+1)
+
+while card.qsize() > 1:
+    card.get()
+    card.put(card.get())
+
+print(card.queue) # 요소가 아닌 deque([4]) 형태로 출력됨
