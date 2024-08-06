600541b600183aa4c07c015d006db693243fcb4d
Jiyun <kjy5952@naver.com>
2024-08-06 21:22:59 +0900

    Baekjoon Divisor 5086

---
+++ Baekjoon/Divisor, Multiple/5086.py
@@ -0,0 +1,13 @@
+#배수와 약수
+while (1):
+    x,y = map(int,input().split())
+    
+    if x==0 and y==0:
+        break
+        
+    if x<y and y%x==0:
+        print("factor")
+    elif x>y and x%y==0:
+        print("multiple")
+    else:
+        print("neither")
